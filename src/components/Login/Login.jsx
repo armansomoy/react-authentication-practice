2c287a7991db85abdca8f0b88a94ce2420ca30ba
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-19 22:08:46 +0600

    Open Google Login Popup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth  } from "firebase/auth";
-import app from '../../firebase/firebase.init';
+import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
-    const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
-    const googleSignIn = () => {
-        console.log('click');
-    }
-    return (
-        <div>
-            <button onClick={googleSignIn}>Google Login</button>
-        </div>
-    );
+  const googleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <button onClick={googleSignIn}>Google Login</button>
+    </div>
+  );
 };
 
 export default Login;
